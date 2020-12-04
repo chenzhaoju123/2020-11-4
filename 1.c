28727e82e00139c4c97aef01755cb853b9832398
chenzhaoju123 <812049449@qq.com>
2020-12-03 19:33:57 -0800

    hahaha

--- 1.c
+++ 1.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 
-int main (int agrc , const char agrv[] ){
-	printf("hello \n");
+int main (int agrc , const char *agrv[] ){
+	printf("hello  \n");
+	printf("lalalal \n");
 	return 0;
 }
